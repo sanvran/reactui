e4674123bb02925f21724702a0e6094a19e7c562
indiandevtm <rajarockromeo@gmail.com>
2022-07-27 11:29:30 +0530

    header part

---
+++ src/component/Main.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Main.css';
+
+ function Main() {
+  return (
+
+<div >
+<div className="container-fluid">
+  <div className='row'>
+<div className='col-lg-5 col-sm-12 col-md-5 'style={{marginTop:'99px'}} >
+
+    <div className='list'>
+    <span><h1>
+      <strong>You’ll never worry</strong></h1>
+      <h2>about money again.</h2></span>
+
+      <span>
+     <p style={{color:'white'}}>
+
+      MyMoneyMantra is your one-stop destination 
+      to search for the best Home Loans, Personal Loans and 
+      Credit Cards in India. Since 1989, more than 
+      50 lakh customers have fulfilled their dreams 
+      with us. Our network of 
+      100+ banks and NBFCs promise 
+      most affordable and suitable finance 
+      deal for you.
+     </p>
+
+      </span>
+      </div>
+</div>
+<div className='col-lg-7  col-sm-12 mt-5 col-md-5'>
+  <img src='l.svg' />
+</div>
+</div>
+</div>
+</div>
+
+
+
+
+
+  )
+}
+export default Main
---
+++ src/component/MainList.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import './MainList.css';
+
+function MainList() {
+
+  return (
+
+    
+    
+    <div>
+    <div className='container'>
+    <div className='row gx-5 text-center"'>
+     <div  className='col-lg-3  '>
+     <img src='sm.svg'/>
+     <div className='list-item'>
+      <h3>
+        50,00000
+        <strong>+</strong>
+      </h3>
+      <p>Happy customer</p>
+     </div>
+     </div>
+
+
+     <div className='col-lg-3 px-14'>
+     <img src='sm.svg'/>
+     <div className='list-item'>
+      <h3>
+        50,00000
+        <strong>+</strong>
+      </h3>
+      <p>Happy customer</p>
+     </div>
+     </div>
+
+     <div className='col-lg-3 px-14'>
+     <img src='sm.svg'/>
+     <div className='list-item'>
+      <h3>
+        50,00000
+        <strong>+</strong>
+      </h3>
+      <p>Happy customer</p>
+     </div>
+     </div>
+
+     <div className='col-lg-3 px-14 .pl-9'>
+     <img src='sm.svg'/>
+     <div className='list-item'>
+      <h3>
+        50,00000
+        <strong>+</strong>
+      </h3>
+      <p>Happy customer</p>
+     </div>
+     </div>
+
+
+    </div>
+
+    </div>
+
+
+    </div>
+  )
+}
+
+export default MainList
